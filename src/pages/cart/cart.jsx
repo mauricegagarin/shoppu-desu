0ee44996bad13dc35ac45a8cd283442929978016
mauricegagarin <146603948+mauricegagarin@users.noreply.github.com>
2024-02-01 13:25:32 +0800

    Add files via upload
    created by Gagarin, Arciaga, Atok and Baniqued

---
+++ src/pages/cart/cart.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+import { PRODUCTS } from "../../products";
+import { CartItem } from "./cart-item";
+import { useNavigate } from "react-router-dom";
+
+import "./cart.css";
+export const Cart = () => {
+  const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="cart">
+    <div>
+      <h1>Your Cart Items</h1>
+    </div>
+    <div className="cart">
+      {PRODUCTS.filter((product) => cartItems[product.id] !== 0).map((product) => (
+        <CartItem data={product} key={product.id} />
+      ))}
+    </div>
+  
+    {totalAmount > 0 ? (
+      <div className="checkout">
+        <p> Subtotal: ${totalAmount} </p>
+        <button onClick={() => navigate("/")}> Continue Shopping </button>
+        <button
+          onClick={() => {
+            checkout();
+            navigate("/checkout");
+          }}
+        >
+          {" "}
+          Checkout{" "}
+        </button>
+      </div>
+    ) : (
+      <h1> Your Shopping Cart is Empty</h1>
+    )}
+   </div>
+  )
+};
---
+++ src/pages/contact.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import'./contact.css';
+
+export const Contact = () => {
+  return <div>
+     <h2>Email Us</h2>
+      <p>Feel free to reach out!</p>
+
+      {/* Developer Information */}
+      <div>
+        <h3>Developer 1</h3>
+        <p>Name: Maurice Gabriel P. Gagarin</p>
+        <p>Course and Section: BSCS 31A1</p>
+        <p>Email: gavpgagarin@gmail.com</p>
+
+        <h3>Developer 2</h3>
+        <p>Name: Marlon Christopher Arciaga</p>
+        <p>Course and Section: BSCS 31A1</p>
+        <p>Email: arciagamarlon@gmail.com</p>
+
+        <h3>Developer 3</h3>
+        <p>Name: Sherwin Kerby F. Atok</p>
+        <p>Course and Section: BSCS 31A1</p>
+        <p>Email: arciagamarlon@gmail.com</p>
+
+        <h3>Developer 4</h3>
+        <p>Name: Kenjie M. Baniqued</p>
+        <p>Course and Section: BSCS 31A1</p>
+        <p>Email: c1750-23@itmlyceumalabang.onmicrosoft.com</p>
+
+
+        
+      </div>
+
+  </div>;
+};
